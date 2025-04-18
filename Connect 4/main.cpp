27bc6584918deb69ced78455233361b05e736c76
Quentin Prazak <quentinprazak@gmail.com>
2025-04-18 16:05:03 -0700

    sfml window working

--- Connect 4/main.cpp
+++ Connect 4/main.cpp
@@ -1,11 +1,36 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
-#include <Balls.hpp>
-#include <Boards.hpp>
+
+int main() {
+    
+    unsigned int width = 640;
+    unsigned int height = 360;
+
+    sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode({ width, height }), "Connect Andy");
+
+    while (window->isOpen()) {
+        while (const std::optional event = window->pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
+
+                window->close();
+            }
+            else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()) {
+                
+                if (keyPressed->scancode == sf::Keyboard::Scancode::Escape) {
+                    
+                    window->close();
+                }
+            }
+        }
+        // Render
+        window->clear(sf::Color(0xFF8800FF));
+
+        // Drawing 
+
+        window->display();
+    }
 
 
-int main(void) {
-
-	std::cout << "Welcome to Connect Andy!" << std::endl;
-
-	return 0;
+    delete window;
+    return 0;
 }
