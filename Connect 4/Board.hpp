c19e3896b0414bcb0ec55dc4cfadf598d08aa9c2
Nathan Rim <rimnat000@gmail.com>
2025-04-18 18:16:14 -0700

    balls and boards displayed

--- Connect 4/Board.hpp
+++ Connect 4/Board.hpp
@@ -5,15 +5,8 @@
 #include <time.h>
 #include <SFML/Graphics.hpp>
 
-class Paddle : public sf::RectangleShape
-{
+class Board{
 public:
-	Paddle(const sf::Vector2f& size, const sf::Vector2f& pos, const sf::Color& color) :
-		sf::RectangleShape(size)
-	{
-		this->setFillColor(color);
-		this->setPosition(pos);
-	}
 
 private:
 };
--- Connect 4/main.cpp
+++ Connect 4/main.cpp
@@ -10,6 +10,11 @@ int main() {
     const sf::Texture texture("boardimage.jpg");
     sf::Sprite sprite(texture);
 
+    // Create a circle shape to represent the ball
+    sf::CircleShape ball(78); // Radius of 80 pixels
+    ball.setFillColor(sf::Color::Green); // Set the color of the ball
+    ball.setPosition({ 703.f, 1077.f }); // Position the ball in the center of the window
+
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
@@ -21,57 +26,8 @@ int main() {
         window.clear();
 
         window.draw(sprite);
+        window.draw(ball);
 
         window.display();
     }
-
-    //sf::RenderWindow window(sf::VideoMode({ 1500, 1500 }), "SFML works!");
-    //sf::CircleShape shape(400.f);
-    //shape.setFillColor(sf::Color::Green);
-    //Paddle p1Paddle(sf::Vector2f(100, 400), sf::Vector2f(0.0, 0.0),
-    //    sf::Color::Magenta),
-    //    p2Paddle(sf::Vector2f(window.getSize().x / 20, window.getSize().y / 5),
-    //        sf::Vector2f(window.getSize().x - window.getSize().x / 20, 0),
-    //        sf::Color::Cyan);
-    //Ball gameBall(p1Paddle.getSize().x / 2,
-    //    sf::Vector2f(window.getSize().x / 2, window.getSize().y / 2),
-    //    sf::Color::Yellow);
-
-    //int direction = 1;
-
-    //// shape.
-
-    //while (window.isOpen())
-    //{
-    //    while (const std::optional event = window.pollEvent())
-    //    {
-    //        if (event->is<sf::Event::Closed>())
-    //            window.close();
-
-    //        //if 
-    //    }
-
-    //    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
-    //    {
-    //        p1Paddle.move({ 0, 0.5 });
-    //    }
-
-    //    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
-    //    {
-    //        p2Paddle.move({ 0, 0.5});
-    //    }
-
-    //    if (p2Paddle.getGlobalBounds().findIntersection(gameBall.getGlobalBounds()))
-    //    {
-    //        direction *= -1;
-    //    }
-
-    //    gameBall.move(sf::Vector2f(0.01 * direction, 0));
-
-    //    window.clear();
-    //    window.draw(p1Paddle);
-    //    window.draw(p2Paddle);
-    //    window.draw(gameBall);
-    //    window.display();
-    //}
 }
