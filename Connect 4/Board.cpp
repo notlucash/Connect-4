dc41811625e0dad7d9229548b4e7d0c3fcc20208
notlucash <lucas.heinrichs@outlook.com>
2025-04-23 23:25:34 -0700

    finishing touches/fixes and willing menu

--- Connect 4/Board.cpp
+++ Connect 4/Board.cpp
@@ -62,6 +62,17 @@ bool Board::isColumnFull(int col)
 }
 
 
+void Board::clearBoard()
+{
+    for (int i = 0; i < ROWS; ++i)
+    {
+        for (int j = 0; j < COLS; ++j)
+        {
+            gameBoard[i][j] = 0;
+        }
+    }
+}
+
 bool Board::checkLine(int player, int r1, int c1, int dr, int dc)
 {
     for (int i = 0; i < 4; i++)
@@ -132,213 +143,276 @@ void Board::runGame()
     window.setFramerateLimit(60);
 
     Menu gameMenu;
-    int menuOption = gameMenu.DisplayMenu(window);
+    int menuOption = 0;
 
-    if (menuOption == 0) return;
-    while (menuOption != 1)
+    do
     {
-        if (menuOption == 2)
+        while (menuOption != 1)
         {
-            
+            menuOption = gameMenu.DisplayMenu(window);
+            if (menuOption == 2)
+            {
+                menuOption = gameMenu.displayRules(window);
+            }
+            if (menuOption == 3)
+            {
+                break;
+            }
         }
-    }
 
+        int current_player = 1;
+        int selected_column = COLS / 2;
+        bool pieceDropping = false;
+        bool gameOver = false;
+        int winner = 0;
+        clearBoard();
 
-    int current_player = 1;
-    int selected_column = COLS / 2;
-    bool pieceDropping = false;
-    bool gameOver = false;
-    int winner = 0;
-
-    sf::Texture boardTexture;
-    if (!boardTexture.loadFromFile("boardimagesmall.jpg"))
-    {
-        std::cout << "Error loading board texture!" << std::endl;
-        return;
-    }
-    sf::Sprite boardSprite(boardTexture);
-
-    sf::Texture transparentTex;
-    if (!transparentTex.loadFromFile("board.png"))
-    {
-        std::cout << "Error loading board texture!" << std::endl;
-        return;
-    }
-    sf::Sprite transparentSprite(transparentTex);
-
-    sf::Font font;
-    if (!font.openFromFile("JungleAdventurer.ttf"))
-    {
-        std::cerr << "Error loading font: JungleAdventurer.ttf" << std::endl;
-
-        if (!font.openFromFile("arial.ttf"))
+        sf::Texture boardTexture;
+        if (!boardTexture.loadFromFile("boardimagesmall.jpg"))
         {
-            std::cout << "Error loading fallback font: arial.ttf" << std::endl;
+            std::cout << "Error loading board texture!" << std::endl;
             return;
         }
-    }
+        sf::Sprite boardSprite(boardTexture);
 
-    sf::Text winText(font, "");
-    winText.setCharacterSize(60);
-    winText.setStyle(sf::Text::Bold);
-    winText.setPosition({ 350.f, 50.f });
-
-    std::vector<Ball> placedBalls;
-    placedBalls.reserve(ROWS * COLS);
-    Ball previewBall(39.3f, { 285.f + selected_column * 105.f, 21.f }, sf::Color::Red);
-
-    sf::Clock clock;
-
-    while (window.isOpen())
-    {
-        float deltaTime = clock.restart().asSeconds();
-
-        while (const std::optional event = window.pollEvent())
+        sf::Texture transparentTex;
+        if (!transparentTex.loadFromFile("board.png"))
         {
-            if (event->is<sf::Event::Closed>())
+            std::cout << "Error loading board texture!" << std::endl;
+            return;
+        }
+        sf::Sprite transparentSprite(transparentTex);
+
+        sf::Font font;
+        if (!font.openFromFile("JungleAdventurer.ttf"))
+        {
+            std::cerr << "Error loading font: JungleAdventurer.ttf" << std::endl;
+
+            if (!font.openFromFile("arial.ttf"))
             {
-                window.close();
+                std::cout << "Error loading fallback font: arial.ttf" << std::endl;
+                return;
             }
+        }
 
-            if (!pieceDropping && !gameOver)
+        sf::Text winText(font, "");
+        winText.setCharacterSize(60);
+        winText.setStyle(sf::Text::Bold);
+
+        std::vector<Ball> placedBalls;
+        placedBalls.reserve(ROWS * COLS);
+        Ball previewBall(39.3f, { 285.f + selected_column * 105.f, 21.f }, sf::Color::Red);
+
+        sf::Clock clock;
+
+        while (window.isOpen())
+        {
+            float deltaTime = clock.restart().asSeconds();
+
+            while (const std::optional event = window.pollEvent())
             {
-                if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
+                if (event->is<sf::Event::Closed>())
                 {
-                    if (keyPressed->code == sf::Keyboard::Key::Left)
-                    {
-                        selected_column = std::max(0, selected_column - 1);
-                    }
-                    else if (keyPressed->code == sf::Keyboard::Key::Right)
-                    {
-                        selected_column = std::min(COLS - 1, selected_column + 1);
-                    }
-                    else if (keyPressed->code == sf::Keyboard::Key::Down ||
-                        keyPressed->code == sf::Keyboard::Key::Enter ||
-                        keyPressed->code == sf::Keyboard::Key::Space)
-                    {
+                    window.close();
+                }
 
-                        if (!isColumnFull(selected_column))
+                if (!pieceDropping && !gameOver)
+                {
+                    if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
+                    {
+                        if (keyPressed->code == sf::Keyboard::Key::Left)
                         {
-                            int row = getFirstEmptyRow(selected_column);
-                            updateBoard(row, selected_column, current_player);
+                            selected_column = std::max(0, selected_column - 1);
+                        }
+                        else if (keyPressed->code == sf::Keyboard::Key::Right)
+                        {
+                            selected_column = std::min(COLS - 1, selected_column + 1);
+                        }
+                        else if (keyPressed->code == sf::Keyboard::Key::Down ||
+                            keyPressed->code == sf::Keyboard::Key::Enter ||
+                            keyPressed->code == sf::Keyboard::Key::Space)
+                        {
 
-                            float targetX = 285.f + selected_column * 105.f;
-                            float targetY = 181.5 + row * 99;
-                            sf::Color currentPlayerColor;
-
-                            if (current_player == 1)
+                            if (!isColumnFull(selected_column))
                             {
-                                currentPlayerColor = sf::Color::Red;
-                            }
-                            else
-                            {
-                                currentPlayerColor = sf::Color::Yellow;
-                            }
+                                int row = getFirstEmptyRow(selected_column);
+                                updateBoard(row, selected_column, current_player);
 
+                                float targetX = 285.f + selected_column * 105.f;
+                                float targetY = 181.5 + row * 99;
+                                sf::Color currentPlayerColor;
 
-                            placedBalls.emplace_back(39.6f, sf::Vector2f{ targetX, 21.f }, currentPlayerColor);
-                            placedBalls.back().dropBall(targetY);
-                            pieceDropping = true;
-
-                            if (checkWin(current_player))
-                            {
-                                gameOver = true;
-                                winner = current_player;
-                            }
-                            else
-                            {
-                                bool boardFull = true;
-                                for (int c = 0; c < COLS; c++)
-                                {
-                                    if (!isColumnFull(c))
-                                    {
-                                        boardFull = false;
-                                        break;
-                                    }
-                                }
-                                if (boardFull)
-                                {
-                                    gameOver = true;
-                                    winner = 3;
-                                }
-                            }
-
-                            if (!gameOver)
-                            {
                                 if (current_player == 1)
                                 {
-                                    current_player = 2;
-                                    previewBall.setFillColor(sf::Color::Red);
+                                    currentPlayerColor = sf::Color::Red;
                                 }
                                 else
                                 {
-                                    current_player = 1;
-                                    previewBall.setFillColor(sf::Color::Yellow);
+                                    currentPlayerColor = sf::Color::Yellow;
                                 }
-                            }
 
-                        }
-                        else
-                        {
-                            std::cout << "Column " << selected_column << " is full!" << std::endl;
+
+                                placedBalls.emplace_back(39.6f, sf::Vector2f{ targetX, 21.f }, currentPlayerColor);
+                                placedBalls.back().dropBall(targetY);
+                                pieceDropping = true;
+
+                                if (checkWin(current_player))
+                                {
+                                    gameOver = true;
+                                    winner = current_player;
+                                }
+                                else
+                                {
+                                    bool boardFull = true;
+                                    for (int c = 0; c < COLS; c++)
+                                    {
+                                        if (!isColumnFull(c))
+                                        {
+                                            boardFull = false;
+                                            break;
+                                        }
+                                    }
+                                    if (boardFull)
+                                    {
+                                        gameOver = true;
+                                        winner = 3;
+                                    }
+                                }
+
+                                if (!gameOver)
+                                {
+                                    if (current_player == 1)
+                                    {
+                                        previewBall.setFillColor(sf::Color::Yellow);
+                                        current_player = 2;
+                                    }
+                                    else
+                                    {
+                                        previewBall.setFillColor(sf::Color::Red);
+                                        current_player = 1;
+                                    }
+                                }
+
+                            }
+                            else
+                            {
+                                std::cout << "Column " << selected_column << " is full!" << std::endl;
+                            }
                         }
                     }
                 }
             }
-        }
 
 
-        if (!gameOver)
-        {
-            previewBall.setPosition({ 285.f + selected_column * 105.f, 21.f });
-        }
-        bool stillDropping = false;
-        for (Ball& ball : placedBalls)
-        {
-            ball.updateBall(deltaTime);
-            if (ball.isDrop)
+            if (!gameOver)
             {
-                stillDropping = true;
+                previewBall.setPosition({ 285.f + selected_column * 105.f, 21.f });
             }
-        }
-        if (pieceDropping && !stillDropping)
-        {
-            pieceDropping = false;
-        }
+            bool stillDropping = false;
+            for (Ball& ball : placedBalls)
+            {
+                ball.updateBall(deltaTime);
+                if (ball.isDrop)
+                {
+                    stillDropping = true;
+                }
+            }
+            if (pieceDropping && !stillDropping)
+            {
+                pieceDropping = false;
+            }
 
-        window.clear(sf::Color(239, 223, 213));
-        window.draw(boardSprite);
-        for (const Ball& ball : placedBalls)
-        {
-            window.draw(ball);
-        }
-        if (!gameOver)
-        {
-            window.draw(previewBall);
-        }
-        window.draw(transparentSprite);
-        if (gameOver)
-        {
-            // Set text based on winner
-            if (winner == 1)
+            window.clear(sf::Color(239, 223, 213));
+            window.draw(boardSprite);
+            for (const Ball& ball : placedBalls)
             {
-                winText.setString("Player 1 (Red) Wins!");
-                winText.setFillColor(sf::Color::Red);
+                window.draw(ball);
             }
-            else if (winner == 2)
+            if (!gameOver)
             {
-                winText.setString("Player 2 (Yellow) Wins!");
-                winText.setFillColor(sf::Color::Yellow);
+                window.draw(previewBall);
             }
-            else if (winner == 3)
+            window.draw(transparentSprite);
+            if (gameOver)
             {
-                winText.setString("It's a Draw!");
-                winText.setFillColor(sf::Color::Black);
+                // Set text based on winner
+                if (winner == 1)
+                {
+                    winText.setString("Player 1 (Red) Wins!");
+                    winText.setFillColor(sf::Color::Red);
+                }
+                else if (winner == 2)
+                {
+                    winText.setString("Player 2 (Yellow) Wins!");
+                    winText.setFillColor(sf::Color::Yellow);
+                }
+                else if (winner == 3)
+                {
+                    winText.setString("It's a Draw!");
+                    winText.setFillColor(sf::Color::Black);
+                }
+                // put win text in the center
+                sf::FloatRect textRect = winText.getLocalBounds();
+                winText.setOrigin(textRect.getCenter());
+                winText.setPosition({ 600.f, 50.f });
+
+                sf::RectangleShape playAgain({ 400.f, 130.f });
+                playAgain.setOrigin(playAgain.getGeometricCenter());
+                playAgain.setFillColor(sf::Color(100, 100, 100));
+                playAgain.setPosition({ 600.f, 300.f });
+
+                sf::Text play(font, "Play Again");
+                play.setCharacterSize(50);
+                play.setStyle(sf::Text::Bold);
+                play.setFillColor(sf::Color::Black);
+                textRect = play.getLocalBounds();
+                play.setOrigin(textRect.getCenter());
+                play.setPosition({ 600.f, 300.f });
+
+
+                sf::RectangleShape endMenu({ 400.f, 130.f });
+                endMenu.setOrigin(endMenu.getGeometricCenter());
+                endMenu.setFillColor(sf::Color(100, 100, 100));
+                endMenu.setPosition({ 600.f, 500.f });
+
+                sf::Text menu(font, "Return to Menu");
+                menu.setCharacterSize(50);
+                menu.setStyle(sf::Text::Bold);
+                menu.setFillColor(sf::Color::Black);
+                textRect = menu.getLocalBounds();
+                menu.setOrigin(textRect.getCenter());
+                menu.setPosition({ 600.f, 500.f });
+
+                window.draw(winText);
+                window.draw(playAgain);
+                window.draw(endMenu);
+                window.draw(play);
+                window.draw(menu);
+
+                sf::FloatRect playButton = playAgain.getGlobalBounds();
+                sf::FloatRect menuButton = endMenu.getGlobalBounds();
+
+                sf::Clock menuCooldown;
+
+                if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+                {
+                    sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+
+                    if (playButton.contains(mousePos))
+                    {
+                        menuOption = 1;
+                        break;
+                    }
+                    if (menuButton.contains(mousePos))
+                    {
+                        menuOption = 0;
+                        break;
+                    }
+                }
             }
-            window.draw(winText);
-        }
-        window.display();
-    } // End main game loop
+            window.display();
+        } // End main game loop
+    } while (menuOption != 3);
 }
 
 
--- Connect 4/Menu.cpp
+++ Connect 4/Menu.cpp
@@ -1,7 +1,10 @@
 #include "Menu.hpp"
 
+#include <Windows.h>;
+
 Menu::Menu() {
 	this->_option = 0;
+	menuTex.loadFromFile("Menu.jpg");
 }
 Menu::~Menu() {
 
@@ -9,7 +12,6 @@ Menu::~Menu() {
 
 int Menu::DisplayMenu(sf::RenderWindow& window) {
 
-	const sf::Texture menuTex("Menu.jpg");
 	sf::Sprite menuSprite(menuTex);
 
 	sf::RectangleShape Button1({ 400.f, 140.f });
@@ -19,18 +21,19 @@ int Menu::DisplayMenu(sf::RenderWindow& window) {
 	sf::RectangleShape Button3({ 400.f, 140.f });
 	Button3.setPosition({ 400.f, 545.f });
 
+	window.clear();
+
 	window.draw(menuSprite);
 
 	window.display();
 
 	while (window.isOpen())
 	{
-
+		Sleep(200);
 		while (const std::optional event = window.pollEvent())
 		{
 			if (event->is<sf::Event::Closed>())
 				window.close();
-
 
 			if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) // if left mouse button is pressed
 			{
@@ -38,28 +41,86 @@ int Menu::DisplayMenu(sf::RenderWindow& window) {
 
 				// get position of buttons
 				sf::FloatRect playButton = Button1.getGlobalBounds();
-				sf::FloatRect helpButton = Button2.getGlobalBounds();
+				sf::FloatRect rulesButton = Button2.getGlobalBounds();
 				sf::FloatRect exitButton = Button3.getGlobalBounds();
 
-				 // if mouse is in local button area
 				if (playButton.contains(mousePos))
 				{
 					return 1;
 				}
-				if (helpButton.contains(mousePos))
+				if (rulesButton.contains(mousePos))
 				{
 					return 2;
 				}
-				// if mouse is in online button area
 				if (exitButton.contains(mousePos))
 				{
 					window.close();
+					return 3;
 				}
 			}
 		}
 	}
 }
 
+int Menu::displayRules(sf::RenderWindow& window)
+{
+	const sf::Font font("JungleAdventurer.ttf");
+
+	sf::Text title(font, "Rules");
+	title.setCharacterSize(75);
+	title.setStyle(sf::Text::Bold);
+	title.setFillColor(sf::Color::White);
+
+	sf::FloatRect textRect = title.getLocalBounds(); // get bounds of text to center
+	title.setOrigin(textRect.getCenter()); // set origin to center
+	title.setPosition({ 600.f, 200.f });
+
+	sf::Text menu(font, "Return to Menu");
+	menu.setCharacterSize(40);
+	menu.setStyle(sf::Text::Bold);
+	menu.setFillColor(sf::Color::White);
+	textRect = menu.getLocalBounds();
+	menu.setOrigin(textRect.getCenter());
+	menu.setPosition({ 600.f, 700.f });
+
+	sf::Text rules(font, "- Players take turns dropping one ball into a column\n- The goal is to get four in a row - vertically, horizontally, or diagonally\n- If the board fills up with no winner, it's a draw\n\n\n\n\n- Use arrow keys to move ball\n- Use down arrow, space, or enter to drop");
+	rules.setCharacterSize(30);
+	rules.setStyle(sf::Text::Bold);
+	rules.setFillColor(sf::Color::White);
+	textRect = rules.getLocalBounds();
+	rules.setOrigin(textRect.getCenter());
+	rules.setPosition({ 600.f, 450.f });
+
+	window.clear();
+
+	window.draw(title);
+    window.draw(menu);
+	window.draw(rules);
+
+	window.display();
+
+	while (window.isOpen())
+	{
+		while (const std::optional event = window.pollEvent())
+		{
+			if (event->is<sf::Event::Closed>())
+				window.close();
+
+			if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+			{
+				sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+
+				sf::FloatRect menuButton = menu.getGlobalBounds(); // get position of text
+
+
+				if (menuButton.contains(mousePos))
+				{
+					return 0;
+				}
+			}
+		}
+	}
+}
 
 // networking was hard
 
